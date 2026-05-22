0e7418220b19780db06e2bc969d81e37555b1b5b
Александр Жданов <sanko_populayr@mail.ru>
2026-05-22 15:45:08 +0300

    Update menu.php

--- components/menu.php
+++ components/menu.php
@@ -1,1 +1,6 @@
-
+<div class="menuConteiner">
+    <a href="/">Главная</a>
+    <a href="/posts.php">Посты</a>
+    <a href="/categories.php">Категории</a>
+    <!-- <a href="/calculator.php">Калькулятор(его не трогал)</a> -->
+</div>
